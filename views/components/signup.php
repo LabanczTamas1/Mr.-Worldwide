a785f9eb39415679d1f8ff54d1b0e76ec2cdcaf1
Labancz Tamás <labancztamas@yahoo.hu>
2024-09-25 21:23:31 +0200

    Bejelentkezés és Regisztrációs oldalak hozzáadva és ezekre a navigáció a home_layout.php-ból

---
+++ views/components/signup.php
@@ -0,0 +1,16 @@
+<div class="signup-container">
+    <h2>Regisztráció</h2>
+    <form action="/signup_handler.php" method="POST" class="signup-filter-container">
+        <!-- Username Input -->
+        <input type="text" id="username" name="username" class="search-input" placeholder="Felhasználónév" required>
+        
+        <!-- Password Input -->
+        <input type="email" id="email" name="email" class="search-input" placeholder="Email" required>
+        
+        <!-- Password Input -->
+        <input type="password" id="password" name="password" class="search-input" placeholder="Jelszó" required>
+        
+        <!-- Submit Button -->
+        <button type="submit">Regisztráció</button>
+    </form>
+</div>
