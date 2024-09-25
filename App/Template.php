6a944b86c947f12d7200eb4c7d831645f812a404
Labancz Tamás <labancztamas@yahoo.hu>
2024-09-25 20:22:17 +0200

    Hozzáadva a Template.php file

---
+++ App/Template.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+include(__DIR__ . '/../config.php');
+
+
+class Template
+{
+    public $layout;
+
+    public $title;
+
+
+
+    function __construct($title = 'Sheetstack', $layout = 'empty')
+    {
+        $layout = $GLOBALS['BASE_DIR'] . '/views/layouts/' . $layout . '.php';
+
+
+        $this->layout = $layout;
+        $this->title = $title;
+
+        ob_start();
+        register_shutdown_function(array($this, 'layout_shutdown'));
+    }
+
+
+
+    public function layout_shutdown()
+    {
+
+        $content = ob_get_contents();
+
+        ob_end_clean();
+
+        if (is_null($this->layout)) {
+            echo $content;
+            return;
+        }
+
+        $title = $this->title;
+        require $this->layout;
+    }
+
+    public function include($path)
+    {
+        return include($GLOBALS['BASE_DIR'] . '/views/' . $path . '.php');
+    }
+    public function asset($path)
+    {
+        $random = $GLOBALS['DEV_MODE'] ? '?' . rand(1, 1200) : '';
+        return '/files/' . $path . $random;
+    }
+}
